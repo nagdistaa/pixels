7a82b5f37df65d8c0751d696192b678b40d8f13d
nagdista <nagdista@gmail.com>
2025-03-26 17:40:37 +0200

    fix media query bug

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-
 const container = document.querySelector(".container");
 
-let containerSize = window.innerWidth*2
+let containerSize = window.innerWidth * 2;
 let pixelSize = 30;
 
-let rowsNumber = containerSize / pixelSize
-let colsNumber = containerSize / pixelSize
+let rowsNumber = containerSize / pixelSize;
+let colsNumber = containerSize / pixelSize;
 
 for (let row = 0; row < rowsNumber; row++) {
-    for (let col = 0; col < colsNumber; col++) {
-        let pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        pixel.style.top = `${row * pixelSize}px`;
-        pixel.style.left = `${col * pixelSize}px`;
-        pixel.onclick = (e)=>{
-            pixel.classList.toggle("active");
-           
-        }
-        container.appendChild(pixel);
-    }
+  for (let col = 0; col < colsNumber; col++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.style.top = `${row * pixelSize}px`;
+    pixel.style.left = `${col * pixelSize}px`;
+    pixel.onclick = (e) => {
+      pixel.classList.toggle("active");
+    };
+    container.appendChild(pixel);
+  }
 }
